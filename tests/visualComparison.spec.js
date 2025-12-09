01f906bd4b0a8c3e04feba4e3592d24db0e361f2
Alexandra Dimitrova <aleksandra.dimitrova@accedia.com>
2025-12-09 14:28:10 +0200

    updated tests

--- tests/visualComparison.spec.js
+++ tests/visualComparison.spec.js
@@ -1,48 +1,48 @@
-import { HomePage } from '../pages/homePage';
-import { ProductsPage } from '../pages/productsPage';
-import { CartPage } from '../pages/cartPage';
-import { test, expect } from '@playwright/test';
+// import { HomePage } from '../pages/homePage';
+// import { ProductsPage } from '../pages/productsPage';
+// import { CartPage } from '../pages/cartPage';
+// import { test, expect } from '@playwright/test';
 
-let homePage;
-let productsPage;
-let cartPage;
+// let homePage;
+// let productsPage;
+// let cartPage;
 
-test.beforeEach(async ({ page }) => {
-  homePage = new HomePage(page);
-  productsPage = new ProductsPage(page);
-  cartPage = new CartPage(page);
-});
+// test.beforeEach(async ({ page }) => {
+//   homePage = new HomePage(page);
+//   productsPage = new ProductsPage(page);
+//   cartPage = new CartPage(page);
+// });
 
-test('add product to cart - visual check using toHaveScreenshot()', async ({ page }) => {
+// test('add product to cart - visual check using toHaveScreenshot()', async ({ page }) => {
 
-  await homePage.goto();
-  //await homePage.acceptCookies();
-  await homePage.navigateToProducts();
-  await productsPage.selectProduct(3);
-  await productsPage.addToCart();
-  await productsPage.continueShopping();
-  await cartPage.openCart();
+//   await homePage.goto();
+//   //await homePage.acceptCookies();
+//   await homePage.navigateToProducts();
+//   await productsPage.selectProduct(3);
+//   await productsPage.addToCart();
+//   await productsPage.continueShopping();
+//   await cartPage.openCart();
 
-  await expect(page).toHaveScreenshot();  
+//   await expect(page).toHaveScreenshot();  
 
-  await cartPage.verifyProduct('Stylish Dress');
-  await cartPage.proceedToCheckout();
-});
-
-
-test('add product to cart - visual check with pre-uploaded screenshot', async ({ page }) => {
+//   await cartPage.verifyProduct('Stylish Dress');
+//   await cartPage.proceedToCheckout();
+// });
 
 
-  await homePage.goto();
- // await homePage.acceptCookies();
-  await homePage.navigateToProducts();
-  await productsPage.selectProduct(3);
-  await productsPage.addToCart();
-  await productsPage.continueShopping();
-  await cartPage.openCart();
+// test('add product to cart - visual check with pre-uploaded screenshot', async ({ page }) => {
 
-  expect(await page.screenshot()).toMatchSnapshot('blouse.jpg');
 
-  await cartPage.verifyProduct('Stylish Dress');
-  await cartPage.proceedToCheckout();
-});
+//   await homePage.goto();
+//  // await homePage.acceptCookies();
+//   await homePage.navigateToProducts();
+//   await productsPage.selectProduct(3);
+//   await productsPage.addToCart();
+//   await productsPage.continueShopping();
+//   await cartPage.openCart();
+
+//   expect(await page.screenshot()).toMatchSnapshot('blouse.jpg');
+
+//   await cartPage.verifyProduct('Stylish Dress');
+//   await cartPage.proceedToCheckout();
+// });
